5944d54caae19604aa249b5254c644486d918204
faizan saiyed <fai1ggj@gmail.com>
2025-07-12 16:51:02 +0530

    addded in host  000

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ from app import app
 
 def run_app():
     try:
-        execute_sql_files()  # Ensure SQL files are executed first
+        execute_sql_files()
     except Exception as e:
-        exit(1)  # Exit the app if there's an error
+        exit(1)
 
     uvicorn.run(
         "app:app",
-        host="127.0.0.1",
-        port=settings.PORT or 8000,  # Default to 8000 if port is not provided
-        timeout_keep_alive=settings.SERVER_TIMEOUT,  
-        reload=False,  # Set to True for development if needed
+        host="0.0.0.0",
+        port=settings.PORT or 8000,
+        timeout_keep_alive=settings.SERVER_TIMEOUT,
+        reload=False,
     )
+
 
 if __name__ == "__main__":
     run_app()
