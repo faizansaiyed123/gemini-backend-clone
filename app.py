f529d427d6dd0b7671b38a80776e12bd30408959
faizan saiyed <fai1ggj@gmail.com>
2025-07-12 14:56:59 +0530

    changes in app file

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-from logging.config import dictConfig
 from src.routers.auth import router as auth_router
 from src.configs.utilites import execute_sql_files
 from src.routers.user import router as user_router
@@ -11,25 +10,14 @@ from src.routers.message import router as message_router
 from src.routers.subscription import router as subscription_router
 
 def configure_routes(app: FastAPI) -> None:
-    """
-    Register all routers on the application instance.
-    """
-
     app.include_router(auth_router, prefix="/auth", tags=["Auth"])
     app.include_router(user_router, prefix="/user", tags=["User"])
     app.include_router(chatroom_router, prefix="/user", tags=["ChatRoom"])
     app.include_router(message_router, prefix="/user", tags=["Message"])
-    app.include_router(subscription_router , prefix="/subscribe", tags=["Subscription"])
-
-
+    app.include_router(subscription_router, prefix="/subscribe", tags=["Subscription"])
 
 def create_app() -> FastAPI:
-    """
-    Create and configure the FastAPI application.
-    """
     app = FastAPI()
-
-    
 
     app.add_middleware(
         CORSMiddleware,
@@ -39,12 +27,13 @@ def create_app() -> FastAPI:
         allow_headers=["*"],
     )
 
-
- 
     configure_routes(app)
 
-    return app
-execute_sql_files() 
+    @app.on_event("startup")
+    async def startup_event():
+        # This runs safely after the app is up
+        execute_sql_files()
 
+    return app
 
 app = create_app()
